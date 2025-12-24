cbbd9693f1b23db1ac4ab86580f9cd7bce52dc2c
teacherCode <96771123+teacherCode@users.noreply.github.com>
2025-12-24 19:23:22 +0200

    Add project files.

---
+++ SchoolPolls/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using SchoolPolls.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolPolls.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SchoolPollsDBEntities db = new SchoolPollsDBEntities();
+
+        // GET: Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        public ActionResult Login(Guid qrToken)
+        {
+            var user = db.Users.FirstOrDefault(u => u.QRCodeToken == qrToken);
+
+            if (user == null)
+            {
+                ViewBag.Error = "Невалиден QR код";
+                return View();
+            }
+
+            Session["UserId"] = user.UserId;
+            Session["Role"] = user.Role;
+            Session["UserName"] = user.FullName;
+
+            return RedirectToAction("Index", "Polls");
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ SchoolPolls/Controllers/PollsController.cs
@@ -0,0 +1,46 @@
+﻿using SchoolPolls.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace SchoolPolls.Controllers
+{
+    public class PollsController : Controller
+    {
+        private SchoolPollsDBEntities db = new SchoolPollsDBEntities();
+
+        public ActionResult Index()
+        {
+            if (Session["UserId"] == null)
+                return RedirectToAction("Login", "Account");
+
+            var polls = db.Polls
+                          .Where(p => p.IsActive)
+                          .OrderByDescending(p => p.CreatedAt)
+                          .ToList();
+
+            return View(polls);
+        }
+
+        // 🔹 DETAILS
+        public ActionResult Details(int id)
+        {
+            if (Session["UserId"] == null)
+                return RedirectToAction("Login", "Account");
+
+            var poll = db.Polls
+                         .Include(p => p.Options)
+                         .FirstOrDefault(p => p.PollId == id);
+
+            if (poll == null)
+                return HttpNotFound();
+
+            return View(poll);
+        }
+    }
+}
+
